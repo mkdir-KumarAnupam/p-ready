dcd255ffe8170dd763462649294ff0a1fafd2d30
probablyKumar <kranupam101@gmail.com>
2025-02-20 17:18:25 +0530

    Changes in global css and layout, stuffed together a mock page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,26 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <div className="flex justify-between items-center p-4 text-xl border-b font-semibold first-letter:color: 'rgba(155, 145, 155, 1)',
+    
+    ">
+      <div>ProReady</div>
+      <div>Sign In</div>
+    </div>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="flex flex-col gap-4">
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
